5c40a88bb37947d3148c6fe2e37c3f03fb2edd4b
diegosiac <diego35502@hotmail.com>
2024-06-25 08:28:37 -0600

    chore: Update user handler and service files

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,1 +1,19 @@
 package user
+
+import (
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	return &Handler{
+		service: NewService(db),
+	}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+}
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,14 +1,6 @@
 package user
 
 import (
-	"e-commerce-back/internal/models"
-	"e-commerce-back/pkg/db"
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"golang.org/x/crypto/bcrypt"
-
 	"gorm.io/gorm"
 )
 
@@ -18,50 +10,4 @@ type Service struct {
 
 func NewService(db *gorm.DB) *Service {
 	return &Service{db: db}
-}
-
-func (s *Service) RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	fmt.Println("RegisterHandler")
-	fmt.Println("RegisterHandler")
-
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	user.Password = string(hashedPassword)
-	if err := db.DB.Create(&user).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func (s *Service) LoginHandler(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	var storedUser models.User
-	if err := db.DB.Where("username = ?", user.Email).First(&storedUser).Error; err != nil {
-		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
-		return
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password)); err != nil {
-		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
